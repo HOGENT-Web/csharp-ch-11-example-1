ac35e9f62f6dd88cc9fe1318348598e83a95853d
Benjamin Vertonghen <benjamin.vertonghen@hogent.be>
2021-12-15 19:58:03 +0100

    Add Project Files

---
+++ PlaywrightTests/CounterTests.cs
@@ -0,0 +1,27 @@
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+
+namespace Example.PlaywrightTests;
+
+[Parallelizable(ParallelScope.Self)]
+public class CounterTests : PageTest
+{
+    [Test]
+    public async Task Clicking_Counter_Updates_Count()
+    {
+        // Navigate to the counter page
+        await Page.GotoAsync($"{TestHelper.ServerBaseUrl}/counter");
+
+        // Wait until the counter page is really there.
+        await Page.WaitForSelectorAsync("h1");
+
+        // Click on counter
+        await Page.ClickAsync("data-test-id=counter-button");
+
+        // Assert
+        var content = await Page.TextContentAsync("p");
+        Assert.AreEqual("Current count: 1", content);
+    }
+}
